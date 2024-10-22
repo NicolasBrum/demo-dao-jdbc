3e16f835d1964485e9bcbc734780ff6789eeb053
Nicolas Brum <nickormy@gmail.com>
2024-10-21 23:48:56 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
         System.out.println(obj);
+
+        Seller s = new Seller(20,"nicolas","nickormy@gmail.com",new Date(2024,5,19),200.0,obj);
+        System.out.println(s);
     }
 }

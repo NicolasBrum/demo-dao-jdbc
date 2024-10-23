d3893247122a3cdcf1fec7b09105c6bb4437ccbd
Nicolas Brum <nickormy@gmail.com>
2024-10-22 22:05:19 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,9 +11,10 @@ public class Program {
     public static void main(String[] args) {
 
         try{
+
+            // Teste findById
             SellerDao sellerDao = DaoFactory.createSellerDao();
             Seller seller = sellerDao.findById(3);
-
             System.out.println(seller);
 
         }catch(Exception e){
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -49,18 +49,8 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
 
             if(rs.next()){
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller sel = new Seller();
-                sel.setId(rs.getInt("Id"));
-                sel.setName(rs.getString("Name"));
-                sel.setBaseSalary(rs.getDouble("BaseSalary"));
-                sel.setBirthDate(rs.getDate("BirthDate"));
-                sel.setEmail(rs.getString("Email"));
-                sel.setDepartment(dep);
-
+                Department dep = this.instantiateDepartment(rs);
+                Seller sel = this.instantiateSeller(rs, dep);
                 return sel;
             }
 
@@ -77,5 +67,23 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public List<Seller> findAll() {
         return List.of();
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller sel = new Seller();
+        sel.setId(rs.getInt("Id"));
+        sel.setName(rs.getString("Name"));
+        sel.setBaseSalary(rs.getDouble("BaseSalary"));
+        sel.setBirthDate(rs.getDate("BirthDate"));
+        sel.setEmail(rs.getString("Email"));
+        sel.setDepartment(dep);
+        return sel;
     }
 }
